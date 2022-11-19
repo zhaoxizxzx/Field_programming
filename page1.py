907d2a606cc598790982d6e44843306bd56edc63
twenty_q <87605129+Gqiaoxian@users.noreply.github.com>
2022-11-19 16:00:46 +0800

    Add files via upload
    图形化界面初始化

---
+++ page1.py
@@ -0,0 +1,12 @@
+from tkinter import *
+root = Tk()
+root.geometry('460x240')
+root.title('自动转换大小写')
+root.configure(bg='#F2F2F2')
+lb1 = Label(root, text='请输入一串大写或小写数字：',font=('楷体',13))
+lb1.place(relx=0.1, rely=0.1, relwidth=0.5, relheight=0.1)
+inp1 = Entry(root)
+inp1.place(relx=0.1, rely=0.2, relwidth=0.8, relheight=0.1)
+btn1 = Button(root, text='转换',font=('楷体',12),bg='#DFDFDF')
+btn1.place(relx=0.7, rely=0.4, relwidth=0.2, relheight=0.1)
+root.mainloop()
